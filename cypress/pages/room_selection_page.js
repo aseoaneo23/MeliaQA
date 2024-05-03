8f9bfbe250a19143038eceb0f9875bef06d260da
aseoaneo23 <aseoaneo22@fpcoruna.afundacion.org>
2024-05-03 12:39:55 +0200

    Ajustar nombres en base a su naturaleza como buena práctica

--- cypress/pages/room_selection_page.js
+++ cypress/pages/room_selection_page.js
@@ -1,7 +1,7 @@
 import { roomDataPage, cities, searchPage } from '../constants'
 import homePage from './homePage'
 
-class roomPage {
+class RoomPage {
 
     elements = {
         getRewardsButton: () => cy.get(roomDataPage.MELIA_REWARDS_BTN),
@@ -13,7 +13,7 @@ class roomPage {
         getCurrencyResult: () => cy.get(roomDataPage.CURRENCY_RESULT_BTN),
         getModalityButton: () => cy.get(roomDataPage.ROOM_MODALITY_BTN),
         getContinueToTariffButton: () => cy.get(roomDataPage.CONTINUE_TO_TARIFF_BTN),
-        getTriffPageUrl: () => cy.url()
+        getTariffPageUrl: () => cy.url()
     }
 
     searchHotel = () => {
@@ -23,22 +23,22 @@ class roomPage {
                 'user-agent': 'axios/0.27.2'
             }
         })
-        homePage.acceptCookies();
+        homePage.acceptCookies();//TODO: Cambiar cuando se integre el step de homePage
     }
 
     clickOnRewardsFilter = (setActive) => {
 
-        if(setActive === 'false'){
+        if (setActive === 'false') {
             this.elements.getRewardsButton().should('be.visible').click()
         }
-        
+
     }
 
     setOrderBy = (sortMehtod) => {
 
         this.elements.getSortByButton().should('be.visible').click()
 
-        if (sortMehtod === 'highest price'){
+        if (sortMehtod === 'highest price') {
             this.elements.getByHighestButton().should('be.visible').click()
         } else {
             this.elements.getByLowestButton().should('be.visible').click()
@@ -48,15 +48,15 @@ class roomPage {
     setCurrency = (currency) => {
         this.elements.getCurrencyButton().should('be.visible').click()
         this.elements.getCurrencyInput().should('be.visible').type(currency)
-        this.elements.getCurrencyResult().should('be.visible').click({force:true})
+        this.elements.getCurrencyResult().should('be.visible').click({ force: true })
     }
 
-    selectRoomModality = () => this.elements.getModalityButton().should('be.visible').click({force:true})
-    
+    selectRoomModality = () => this.elements.getModalityButton().should('be.visible').click({ force: true })
+
     continueToTariff = () => this.elements.getContinueToTariffButton().should('be.visible').click()
 
-    checkRedirection = () =>{
-        this.elements.getTriffPageUrl().should('include', 'checkout')
+    checkRedirection = () => {
+        this.elements.getTariffPageUrl().should('include', 'checkout')
     }
 
 
@@ -64,4 +64,4 @@ class roomPage {
 
 }
 
-module.exports = new roomPage()
+module.exports = new RoomPage()
--- cypress/support/step_definitions/room_page.js
+++ cypress/support/step_definitions/room_page.js
@@ -1,34 +1,34 @@
 import {roomDataPage} from '../../constants'
-import roomPage from '../../pages/roomSelectionPage'
+import RoomPage from '../../pages/room_selection_page'
 
 
 Given('an user on the room selection page with at least one result', () =>{
-     roomPage.searchHotel()
+     RoomPage.searchHotel()
 })
 
 Given('an user that sets Melia Rewards in {string}', (setActive) =>{
-    roomPage.clickOnRewardsFilter(setActive)
+    RoomPage.clickOnRewardsFilter(setActive)
 })
 
 Given('configures a sort by {string}', (sortMethod) =>{
-    roomPage.setOrderBy(sortMethod)
+    RoomPage.setOrderBy(sortMethod)
 
 })
 
 Given('chooses a currency: {string}', (currency) =>{
-    roomPage.setCurrency(currency)
+    RoomPage.setCurrency(currency)
 })
 
 Given('selects a room modality', () =>{
-    roomPage.selectRoomModality()
+    RoomPage.selectRoomModality()
 })
 
 
 When('user continues to tariff', () =>{
-    roomPage.continueToTariff()
+    RoomPage.continueToTariff()
 })
 
 Then('the user is redirected to tariff page', () =>{
-    roomPage.checkRedirection()    
+    RoomPage.checkRedirection()    
 })
 
